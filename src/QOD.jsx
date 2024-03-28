90b7f11727b54131dc835748e68b466318a786f0
rhildred <rhildred@wlu.ca>
2024-03-28 18:59:14 -0400

    add survey

---
+++ src/QOD.jsx
@@ -0,0 +1,51 @@
+// Default V2 theme
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+
+export default function () {
+    const surveyJson = {
+        title: "Lifeguard Safety",
+        showCorrectAnswer: "always",
+        showProgressBar: "bottom",
+        firstPageIsStarted: true,
+        startSurveyText: "Start Quiz",
+        pages: [{
+            elements: [{
+                type: "html",
+                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+            }, {
+                type: "text",
+                name: "username",
+                titleLocation: "hidden",
+                isRequired: true
+            }]
+        }, {
+            elements: [{
+                type: "radiogroup",
+                name: "question1",
+                title: "What is the most common cause of lifeguard injuries?",
+                choices: [
+                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
+                ],
+                correctAnswer: "Slip and fall accidents"
+            }]
+        }]
+    };
+    const survey = new Model(surveyJson);
+
+    survey.onComplete.add(function (sender) {
+        var questions = sender.getAllQuestions();
+        for (var i = 0; i < questions.length; i++) {
+            var question = questions[i];
+            var correctAnswer = question.correctAnswer;
+            var userAnswer = question.value;
+            var questionTitle = question.title;
+            console.log("Question: " + questionTitle);
+            console.log("Correct Answer: " + correctAnswer);
+            console.log("User Answer: " + userAnswer);
+        }
+    });
+
+    return <Survey model={survey} />;
+}
